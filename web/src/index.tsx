7d93bf9acc3c0f461b6f64a84a97926b1b7d06d1
bastienxs <bastienxs@gmail.com>
2021-04-16 18:41:07 +0200

    routing

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -1,17 +1,16 @@
-
 import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
+import { Routes } from "./Routes";
 
 const client = new ApolloClient({
-  cache : new InMemoryCache(),
+  cache: new InMemoryCache(),
   uri: "http://localhost:5000/graphql",
 });
 
 ReactDOM.render(
   <ApolloProvider client={client}>
-    <App />
+    <Routes />
   </ApolloProvider>,
   document.getElementById("root")
 );
---
+++ web/src/pages/Home.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+interface HomeProps {}
+
+export const Home: React.FC<HomeProps> = ({}) => {
+  return (
+    <div>
+     home page
+    </div>
+  );
+};
